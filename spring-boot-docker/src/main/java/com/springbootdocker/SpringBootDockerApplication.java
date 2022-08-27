0c01d3e610d2438c3331a3c9b6d707336440fb43
hersh <hersh@Hersh>
2022-08-28 02:59:36 +0530

    third

--- spring-boot-docker/src/main/java/com/springbootdocker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/springbootdocker/SpringBootDockerApplication.java
@@ -8,6 +8,7 @@ public class SpringBootDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerApplication.class, args);
+		System.out.println("Started SBA");
 	}
 
 }
