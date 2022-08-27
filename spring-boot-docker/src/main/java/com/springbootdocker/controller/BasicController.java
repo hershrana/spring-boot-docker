86ae7264f74360a4ae6f94f74435cb648db7f56d
hersh <hersh@Hersh>
2022-08-28 01:37:26 +0530

    first

---
+++ spring-boot-docker/src/main/java/com/springbootdocker/controller/BasicController.java
@@ -0,0 +1,25 @@
+package com.springbootdocker.controller;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+@GetMapping(path = {"hi","/hi/{nm}"})
+public String getHi(@PathVariable(name = "nm",required = false) Optional<String> name )
+{
+	
+	return "Hi "+name.orElse(" World ")+" !";
+}
+
+@GetMapping(path = {"hello","/hello/{nm}"})
+public String getHello(@PathVariable(name = "nm",required = false) Optional<String> name )
+{
+	
+	return "Hello "+name.orElse(" World ")+" !";
+}
+
+}
